b5ce668f714b640ac4863406db581c2b319701e9
Lucas Achaval <achaval.lucas@gmail.com>
2021-02-25 15:35:23 -0300

    MovieCarousel js file

---
+++ src/components/MovieCarousel/MovieCarousel.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../../styles/MovieCarousel.css";
+
+function MovieCarousel() {
+  return <div className="movie-carousel"></div>;
+}
+
+export default MovieCarousel;
