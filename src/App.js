98e8ca81d87a55bb55b92d9d6f28596a48c7e07f
Lucas Achaval <achaval.lucas@gmail.com>
2021-02-25 15:31:19 -0300

    App js file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header/Header";
+import MovieCarousel from "./components/MovieCarousel/MovieCarousel";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <MovieCarousel />
+    </div>
+  );
+}
+
+export default App;
