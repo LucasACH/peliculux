1d429ebf14be974405f600c19018a0c3d55ae3b8
Lucas Achaval <achaval.lucas@gmail.com>
2021-02-25 22:31:33 -0300

    MovieSpecs js file

---
+++ src/components/shared/MovieSpecs.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Rating from "@material-ui/lab/Rating";
+
+import "./styles/MovieSpecs.css";
+
+function MovieSpecs({ releaseYear, runtime, rating }) {
+  return (
+    <div className="movie-specs">
+      <p>{releaseYear}</p>
+      <div className="dot"></div>
+      <p>{runtime}</p>
+      <div className="dot"></div>
+      <p>{rating}</p>
+      <Rating name="read-only" precision={0.1} value={rating * 0.5} />
+    </div>
+  );
+}
+
+export default MovieSpecs;
