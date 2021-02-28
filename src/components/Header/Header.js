51381705cf065109bc1c47202199eed5e880c4d0
Lucas Achaval <achaval.lucas@gmail.com>
2021-02-27 23:27:32 -0300

    add fixed Header when scrolling down

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import PrimaryButton from "../shared/PrimaryButton";
 import TextButton from "../shared/TextButton";
@@ -9,20 +9,28 @@ import SearchIcon from "@material-ui/icons/Search";
 import SettingsIcon from "@material-ui/icons/Settings";
 import BookmarkIcon from "@material-ui/icons/Bookmark";
 import CodeIcon from "@material-ui/icons/Code";
+import FilterListIcon from "@material-ui/icons/FilterList";
 
 import "../../styles/Header.css";
 
 function Header() {
+  const [isFixed, setIsFixed] = useState(false);
+
   const getPageYOffset = () => {
-    const marker = window.innerHeight * 0.7;
-    if (window.pageYOffset > marker) return true;
-    return false;
+    const marker = window.innerHeight * 0.5;
+    if (window.pageYOffset > marker) return setIsFixed(true);
+    return setIsFixed(false);
   };
 
-  window.addEventListener("scroll", getPageYOffset);
+  useEffect(() => {
+    window.addEventListener("scroll", getPageYOffset);
+    return () => {
+      window.removeEventListener("scroll", getPageYOffset);
+    };
+  }, []);
 
   return (
-    <div className="header">
+    <div className={isFixed ? "header fixed" : "header"}>
       <div className="left">
         <h1>PELICULUX</h1>
       </div>
@@ -31,23 +39,33 @@ function Header() {
         <input type="text" placeholder="Search movies" />
       </div>
       <div className="right">
-        <Tooltip title="Source code">
-          <IconButton>
-            <CodeIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Saved movies">
-          <IconButton>
-            <BookmarkIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Settings">
-          <IconButton>
-            <SettingsIcon />
-          </IconButton>
-        </Tooltip>
-        <TextButton input="Log In" />
-        <PrimaryButton input="Sign Up" />
+        {isFixed ? (
+          <Tooltip title="Filter">
+            <IconButton>
+              <FilterListIcon />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <>
+            <Tooltip title="Source code">
+              <IconButton>
+                <CodeIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Saved movies">
+              <IconButton>
+                <BookmarkIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Settings">
+              <IconButton>
+                <SettingsIcon />
+              </IconButton>
+            </Tooltip>
+            <TextButton input="Log In" />
+            <PrimaryButton input="Sign Up" />
+          </>
+        )}
       </div>
     </div>
   );
