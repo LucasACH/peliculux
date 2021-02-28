6c379605326122b930d87d8624c858ae8ed531b0
Lucas Achaval <achaval.lucas@gmail.com>
2021-02-28 20:27:48 -0300

    Add DataProvider to elements tree

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Header from "./components/Header/Header";
 import MovieCarousel from "./components/MovieCarousel/MovieCarousel";
 import MoviesGrid from "./components/MoviesGrid/MoviesGrid";
+import { DataProvider } from "./context/DataProvider";
 import "./styles/App.css";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <MovieCarousel />
-      <MoviesGrid />
-    </div>
+    <DataProvider>
+      <div className="app">
+        <Header />
+        <MovieCarousel />
+        <MoviesGrid />
+      </div>
+    </DataProvider>
   );
 }
 
