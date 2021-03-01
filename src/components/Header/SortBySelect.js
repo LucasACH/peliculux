da9aa3f5bed02270c6f56e65be3895479cc0179e
Lucas Achaval <achaval.lucas@gmail.com>
2021-02-28 21:37:42 -0300

    Creating a sortBy select dropdown menu

---
+++ src/components/Header/SortBySelect.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+function SortBySelect() {
+  return (
+    <FormControl margin="normal">
+      <Select
+        disableUnderline={true}
+        // value={sortBy}
+        // onChange={handleChange}
+      >
+        <MenuItem value="popularity.desc">Popularity Descending</MenuItem>
+        <MenuItem value="popularity.asc">Popularity Ascending</MenuItem>
+        <MenuItem value="vote_average.desc">Rating Descending</MenuItem>
+        <MenuItem value="vote_average.asc">Rating Ascending</MenuItem>
+        <MenuItem value="release_date.desc">Release Date Descending</MenuItem>
+        <MenuItem value="release_date.asc">Release Date Ascending</MenuItem>
+        <MenuItem value="original_title.desc">Title (A-Z)</MenuItem>
+        <MenuItem value="original_title.asc">Title (Z-A)</MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
+export default SortBySelect;
