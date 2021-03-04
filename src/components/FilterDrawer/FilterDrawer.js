56808d0eb9c464785a0b5a12ec24628fe04bd31b
Lucas Achaval <achaval.lucas@gmail.com>
2021-03-03 23:46:35 -0300

    Creating filter item div and adding dividers

--- src/components/FilterDrawer/FilterDrawer.js
+++ src/components/FilterDrawer/FilterDrawer.js
@@ -1,9 +1,28 @@
+import { Divider } from "@material-ui/core";
 import React from "react";
 
 import "../../styles/FilterDrawer.css";
 
 function FilterDrawer({ open }) {
-  return <div className={open ? "filter-drawer open" : "filter-drawer"}></div>;
+  return (
+    <div className={open ? "filter-drawer open" : "filter-drawer"}>
+      <div className="filter-item">
+        <p>Genres</p>
+      </div>
+      <Divider />
+      <div className="filter-item">
+        <p>Rating</p>
+      </div>
+      <Divider />
+      <div className="filter-item">
+        <p>Runtime</p>
+      </div>
+      <Divider />
+      <div className="filter-item">
+        <p>Release Date</p>
+      </div>
+    </div>
+  );
 }
 
 export default FilterDrawer;
