ba687e76acac5382da8b2e1103e623eb24231d44
Lucas Achaval <achaval.lucas@gmail.com>
2021-03-04 23:41:54 -0300

    Adding genres chip soup to filter drawer

--- src/components/FilterDrawer/FilterDrawer.js
+++ src/components/FilterDrawer/FilterDrawer.js
@@ -2,12 +2,14 @@ import { Divider } from "@material-ui/core";
 import React from "react";
 
 import "../../styles/FilterDrawer.css";
+import Genres from "./components/Genres";
 
 function FilterDrawer({ open }) {
   return (
     <div className={open ? "filter-drawer open" : "filter-drawer"}>
       <div className="filter-item">
         <h4>Genres</h4>
+        <Genres />
       </div>
       <Divider />
       <div className="filter-item">
