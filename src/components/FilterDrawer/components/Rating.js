70ddc9861419dffe07618b2b946d20180fde1cb3
Lucas Achaval <achaval.lucas@gmail.com>
2021-03-06 20:29:44 -0300

    Creating rating slider component

--- src/components/FilterDrawer/components/Rating.js
+++ src/components/FilterDrawer/components/Rating.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import Slider from "@material-ui/core/Slider";
+
 function Rating() {
-  return <div></div>;
+  return (
+    <div className="rating">
+      <Slider
+        defaultValue={30}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={1}
+        marks
+        min={1}
+        max={10}
+      />
+    </div>
+  );
 }
 
 export default Rating;
