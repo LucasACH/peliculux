fc7a898a6056b61b05a8ed7b10050fc2f0a00d50
Lucas Achaval <achaval.lucas@gmail.com>
2021-03-08 20:06:43 -0300

    Adding react router to app tree

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import Header from "./components/Header/Header";
 import MovieCarousel from "./components/MovieCarousel/MovieCarousel";
 import MoviesGrid from "./components/MoviesGrid/MoviesGrid";
 import { DataProvider } from "./context/DataProvider";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./styles/App.css";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
 
 function App() {
   return (
     <DataProvider>
       <div className="app">
-        <Header />
-        <MovieCarousel />
-        <MoviesGrid />
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/about"></Route>
+            <Route path="/movie/:movie_id">
+              <MovieDetails />
+            </Route>
+            <Route path="/">
+              <MovieCarousel />
+              <MoviesGrid />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </DataProvider>
   );
