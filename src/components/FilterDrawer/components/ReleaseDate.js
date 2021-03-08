902f63eba06d8d2ae2ed38cb2d0ae99e4cc4bdbc
Lucas Achaval <achaval.lucas@gmail.com>
2021-03-08 18:19:26 -0300

    Changing range date picker style

--- src/components/FilterDrawer/components/ReleaseDate.js
+++ src/components/FilterDrawer/components/ReleaseDate.js
@@ -6,30 +6,72 @@ import LocalizationProvider from "@material-ui/lab/LocalizationProvider";
 import Box from "@material-ui/core/Box";
 import MobileDateRangePicker from "@material-ui/lab/MobileDateRangePicker";
 
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+
 import "../styles/ReleaseDate.css";
+
+const theme = createMuiTheme({
+  components: {
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          color: "#ffffff",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          color: "#ffffff",
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          background: "#1c2028",
+          color: "#ffffff",
+          borderRadius: "10px",
+        },
+      },
+    },
+  },
+  palette: {
+    text: {
+      primary: "#ffffff",
+      secondary: "#ffffff",
+    },
+    primary: {
+      main: "#0ce8ce",
+    },
+  },
+});
 
 function ReleaseDate() {
   const [releaseDate, setReleaseDate] = useState([null, null]);
 
   return (
     <div className="release-date">
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <MobileDateRangePicker
-          startText="From"
-          endText="To"
-          value={releaseDate}
-          onChange={(newValue) => {
-            setReleaseDate(newValue);
-          }}
-          renderInput={(startProps, endProps) => (
-            <React.Fragment>
-              <TextField {...startProps} variant="standard" />
-              <Box sx={{ mx: 2 }}> to </Box>
-              <TextField {...endProps} variant="standard" />
-            </React.Fragment>
-          )}
-        />
-      </LocalizationProvider>
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <MobileDateRangePicker
+            startText="From"
+            endText="To"
+            value={releaseDate}
+            onChange={(newValue) => {
+              setReleaseDate(newValue);
+            }}
+            renderInput={(startProps, endProps) => (
+              <React.Fragment>
+                <TextField {...startProps} />
+                <Box sx={{ mx: 2 }}> to </Box>
+                <TextField {...endProps} />
+              </React.Fragment>
+            )}
+          />
+        </LocalizationProvider>
+      </ThemeProvider>
     </div>
   );
 }
