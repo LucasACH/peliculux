e79f54af0523943810bc364a6f1fbe0344150c7f
Lucas Achaval <achaval.lucas@gmail.com>
2021-03-08 17:09:05 -0300

    Adding realese date range picker

--- src/components/FilterDrawer/components/ReleaseDate.js
+++ src/components/FilterDrawer/components/ReleaseDate.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+
+import TextField from "@material-ui/core/TextField";
+import AdapterDateFns from "@material-ui/lab/AdapterDateFns";
+import LocalizationProvider from "@material-ui/lab/LocalizationProvider";
+import Box from "@material-ui/core/Box";
+import MobileDateRangePicker from "@material-ui/lab/MobileDateRangePicker";
+
+import "../styles/ReleaseDate.css";
 
 function ReleaseDate() {
-  return <div></div>;
+  const [releaseDate, setReleaseDate] = useState([null, null]);
+
+  return (
+    <div className="release-date">
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <MobileDateRangePicker
+          startText="From"
+          endText="To"
+          value={releaseDate}
+          onChange={(newValue) => {
+            setReleaseDate(newValue);
+          }}
+          renderInput={(startProps, endProps) => (
+            <React.Fragment>
+              <TextField {...startProps} variant="standard" />
+              <Box sx={{ mx: 2 }}> to </Box>
+              <TextField {...endProps} variant="standard" />
+            </React.Fragment>
+          )}
+        />
+      </LocalizationProvider>
+    </div>
+  );
 }
 
 export default ReleaseDate;
