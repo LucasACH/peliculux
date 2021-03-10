e2e7104b4c81f48b58bf0aaa55e1d4d63c61e166
Lucas Achaval <achaval.lucas@gmail.com>
2021-03-10 16:23:09 -0300

    Add a handle click action to button

--- src/components/shared/YoutubeButton.js
+++ src/components/shared/YoutubeButton.js
@@ -2,10 +2,10 @@ import React from "react";
 import YouTubeIcon from "@material-ui/icons/YouTube";
 import { IconButton, Tooltip } from "@material-ui/core";
 
-function YoutubeButton() {
+function YoutubeButton({ handleClick }) {
   return (
     <Tooltip title="Watch trailer">
-      <IconButton className="save-button">
+      <IconButton className="save-button" onClick={handleClick}>
         <YouTubeIcon />
       </IconButton>
     </Tooltip>
