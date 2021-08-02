fefd9ffa6c4afb074d2b6534201f26071c49d306
Lucas Achaval <achaval.lucas@gmail.com>
2021-08-02 14:42:08 -0300

    Bug fix

--- src/context/popularMovies.js
+++ src/context/popularMovies.js
@@ -13,12 +13,24 @@ export const PopularMoviesProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
 
   const [sortBy, setSortBy] = useState("popularity.desc");
+  const [genres, setGenres] = useState([]);
+  const [rating, setRating] = useState([1, 10]);
+  const [runtime, setRuntime] = useState([1, 360]);
 
   useEffect(() => {
     axios({
       method: "GET",
       url: "https://api.themoviedb.org/3/discover/movie",
-      params: { api_key: API_KEY, page: page, sort_by: sortBy },
+      params: {
+        api_key: API_KEY,
+        page: page,
+        sort_by: sortBy,
+        with_genres: genres.join(", "),
+        "vote_average.gte": rating[0],
+        "vote_average.lte": rating[1],
+        "with_runtime.gte": runtime[0],
+        "with_runtime.lte": runtime[1],
+      },
     })
       .then((response) => {
         setLoading(false);
@@ -26,12 +38,13 @@ export const PopularMoviesProvider = ({ children }) => {
         setFirstTimeLoading(false);
       })
       .catch((error) => setError(error));
-  }, [page, sortBy]);
+  }, [page, sortBy, genres, rating, runtime]);
 
   return (
     <PopularMoviesContext.Provider
       value={{
         loading,
+        setLoading,
         error,
         firstTimeLoading,
         setPage,
@@ -39,6 +52,12 @@ export const PopularMoviesProvider = ({ children }) => {
         setMovies,
         sortBy,
         setSortBy,
+        genres,
+        setGenres,
+        rating,
+        setRating,
+        runtime,
+        setRuntime,
       }}
     >
       {children}
--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,15 +1,19 @@
 function formatDate(releaseDate) {
-  if (releaseDate !== undefined) {
-    const date = new Date(releaseDate);
+  try {
+    if (releaseDate !== undefined) {
+      const date = new Date(releaseDate);
 
-    let year = new Intl.DateTimeFormat("en", {
-      year: "numeric",
-    }).format(date);
-    let month = new Intl.DateTimeFormat("en", { month: "long" }).format(date);
-    let day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+      let year = new Intl.DateTimeFormat("en", {
+        year: "numeric",
+      }).format(date);
+      let month = new Intl.DateTimeFormat("en", { month: "long" }).format(date);
+      let day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
 
-    return `${month} ${day}, ${year}`;
-  } else {
+      return `${month} ${day}, ${year}`;
+    } else {
+      return "";
+    }
+  } catch {
     return "";
   }
 }
